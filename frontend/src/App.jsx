7eac85e75a43ee61280b36c2d354b6247d0962df
Vansh Deshmukh <vanshdeshmukh708@gmail.com>
2026-05-06 14:32:08 +0530

    Initial project setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Tournaments from './pages/Tournaments';
 import Dashboard from './pages/Dashboard';
-import TournamentChat from './pages/TournamentChat';
+import TournamentChat from './pages/TournamentChat'   ;
 
 function App() {
   return (
